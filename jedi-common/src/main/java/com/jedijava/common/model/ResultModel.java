da4b31a73ce1038d49511571e9bb66ffde2b5fc7
kelefun <kelefun@foxmail.com>
2018-04-08 14:25:28 +0800

    add result model

---
+++ jedi-common/src/main/java/com/jedijava/common/model/ResultModel.java
@@ -0,0 +1,63 @@
+package com.jedijava.common.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jedijava.common.result.DefaultResultCode;
+import com.jedijava.common.result.ResultCode;
+
+import java.io.Serializable;
+
+/**
+ * @author liukaiyang
+ * @date 2018/4/8 11:17
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResultModel<T> implements Serializable {
+
+    private static final long serialVersionUID = -5868923397737067940L;
+
+    private boolean success=true;
+    private T data;
+    @JsonIgnore
+    private ResultCode resultCode= DefaultResultCode.SUCCESS;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ResultCode getResultCode() {
+        return resultCode;
+    }
+
+    @JsonProperty("code")
+    public int getCode(){
+        return  resultCode.getCode();
+    }
+
+    @JsonProperty("msg")
+    public String getMsg(){
+        return resultCode.getMsg();
+    }
+
+    public void setResultCode(ResultCode resultCode) {
+        this.resultCode = resultCode;
+    }
+    public static <T> ResultModel<T> success(T data) {
+        ResultModel<T> r = new ResultModel<T>();
+        r.setData(data);
+        return r;
+    }
+}
